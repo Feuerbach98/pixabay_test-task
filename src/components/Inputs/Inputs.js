58b03c3b1dc16029f94956f8031d0afa3c89f7e2
Nikolay Filiuk <Filiuk98@gmail.com>
2020-11-30 17:00:23 +0200

    Optimized code

---
+++ src/components/Inputs/Inputs.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { actions } from '../../store/store';
+
+export const Inputs = ({ query, perPage }) => {
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    dispatch(actions.change(event.target.name, event.target.value))
+  }
+  
+  const handleSubmit = (event) => {
+    if (event.charCode === 13) {
+      event.preventDefault();
+    }
+  }
+
+  return (
+    <Form className="d-flex d-flex justify-content-between">
+      <Form.Group>
+        <Form.Control
+          name="query"
+          type="search"
+          value={query}
+          onChange={handleChange}
+          onKeyPress={handleSubmit}
+          placeholder="Search"
+        />
+      </Form.Group>
+      <Form.Group className="d-flex align-items-center">
+        <Form.Label className="my-0 mx-2">Count:</Form.Label>
+        <Form.Control
+          className="count"
+          as="select"
+          value={perPage}
+          onChange={handleChange}
+          name="perPage"
+        >
+          <option value={10}>10</option>
+          <option value={20}>20</option>
+          <option value={50}>50</option>
+          <option value={100}>100</option>
+        </Form.Control>
+      </Form.Group>
+    </Form>
+  );
+}
--- src/store/store.js
+++ src/store/store.js
@@ -6,6 +6,18 @@ const initialState = {
   perPage: 10,
 }
 
+export const actions = {
+  change: (name, value) => ({
+    type: 'ON_CHANGE',
+    name,
+    value,
+  }),
+  search: async(getImages, query, perPage) => ({
+    type: 'ON_SEARCH',
+    response: await getImages(query, perPage)
+  })
+}
+
 const reducer = (state, action) => {
   switch (action.type) {
     case 'ON_CHANGE': {
